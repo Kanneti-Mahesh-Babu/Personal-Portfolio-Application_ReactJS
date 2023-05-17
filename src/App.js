20985fc5a26d6b7e5ce8258510c875c4fc88b1f1
k@nneti-mahesh-babu <90685474+Kanneti-Mahesh-Babu@users.noreply.github.com>
2023-05-17 20:12:27 +0530

    code pushed
    latest code commit...

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Home from './Home.js'
+
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
